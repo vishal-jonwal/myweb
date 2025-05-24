171ae2e7f5ebbcf3f4efb18794f54c2f16d829f4
vishal-jonwal <vjonwal3@gmail.com>
2025-05-24 17:15:03 +0530

    message

---
+++ Css/CssProject/deepawali/app.js
@@ -0,0 +1,10 @@
+// Q. sum of two  given is a = 10 and b = 5 
+
+// let a = 10;
+// let b = 5;
+// let sum = a+b;
+// console.log(sum);
+// Q. If a circle has a radius of 7 cm, what is its area?
+let r = 7;
+let area = 3.14 * r * r;
+console.log(area);
